4cd60d686328d653acbfda0ed8a4d761b9cbc547
Daria2945 <148208175+Daria2945@users.noreply.github.com>
2025-06-27 16:43:00 +0300

    Initial commit

---
+++ Weapon/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Weapon
+{
+    private int _damage;
+    private int _bullets;
+
+    public Weapon(int damage, int bullets)
+    {
+        if (damage < 0)
+            throw new ArgumentOutOfRangeException(nameof(damage));
+
+        if (bullets < 0)
+            throw new ArgumentOutOfRangeException(nameof(bullets));
+
+        _damage = damage;
+        _bullets = bullets;
+    }
+
+    public void Fire(Player player)
+    {
+        if (_bullets == 0)
+            return;
+
+        _bullets--;
+        player.TakeDamage(_damage);
+    }
+}
+
+class Player
+{
+    private int _health;
+
+    public Player(int health)
+    {
+        if (health <= 0)
+            throw new ArgumentOutOfRangeException(nameof(health));
+
+        _health = health;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (damage < 0)
+            return;
+
+        _health -= damage;
+
+        if (_health <= 0)
+            _health = 0;
+    }
+}
+
+class Bot
+{
+    private Weapon _weapon;
+
+    public Bot(Weapon weapon)
+    {
+        _weapon = weapon;
+    }
+
+    public void OnSeePlayer(Player player)
+    {
+        _weapon.Fire(player);
+    }
+}
